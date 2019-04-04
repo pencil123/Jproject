f0bda0d36748bf6d5d69534750cdc452cf3ffb1b
admin <seeyouon26@outlook.com>
2019-04-05 00:45:36 +0800

    数据库配置信息使用yaml文件解析

--- src/main/java/com/good/month4/NewMySQLConn.java
+++ src/main/java/com/good/month4/NewMySQLConn.java
@@ -1,12 +1,17 @@
 package com.good.month4;
 import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
 
+import java.io.*;
+import org.yaml.snakeyaml.Yaml;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import static java.sql.DriverManager.*;
 
 /**
  * @author lyzhang
@@ -14,10 +19,21 @@ import java.util.List;
  */
 public class NewMySQLConn {
 
+    public static Map readFile() throws FileNotFoundException{
+        Yaml yaml = new Yaml();
+        //String configfile = ".\\src\\main\\java\\com\\good\\month4\\config.yaml";
+        String configfile = "config.yaml";
+        File f = new File(configfile);
+        Map result = (Map) yaml.load(new FileInputStream(f));
+        return (Map) result.get("MySQL");
+    }
+
   public static Connection getConn() throws Exception {
+      Map connConfig = readFile();
       Class.forName("com.mysql.jdbc.Driver");
-      Connection conn= DriverManager.getConnection("jdbc:mysql://12.168.3.75:3306/test_lyzhang", "dba", "dba?ylh79ak");
-      return conn;
+      try (Connection conn = (Connection) getConnection((String) connConfig.get("url"), (String) connConfig.get("user"),(String) connConfig.get("password"))) {
+          return conn;
+      }
   }
 
   public static void closeConn(Connection conn ) throws Exception {
@@ -34,7 +50,7 @@ public class NewMySQLConn {
       }
   }
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
       try{
           getConn();
           System.out.println("MySQL connection sucession;");
