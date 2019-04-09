13312ca3d2bab780f2bd8ccd2262fc83ef42f489
张鲁燕 <zhangluyan@jsh.com>
2019-04-09 16:37:30 +0800

    eclipse git first file

---
+++ src/main/java/com/good/git/Application.java
@@ -0,0 +1,13 @@
+package com.good.git;
+
+import org.eclipse.jgit.api.*;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.transport.PushResult;
+import org.eclipse.jgit.transport.RefSpec;
+import org.eclipse.jgit.transport.RemoteRefUpdate;
+/**
+ * @author lyzhang
+ * @since 2019/4/9 16:26
+ */
+public class Application {
+}
