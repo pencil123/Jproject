93cc1596abba70eabf930904bf32d1bd8392e5fe
admin <seeyouon26@outlook.com>
2019-04-28 23:31:52 +0800

    DOM 遍历获取POM文件的Version

--- src/main/java/com/blogs/Application.java
+++ src/main/java/com/blogs/Application.java
@@ -1,7 +1,9 @@
 package com.blogs;
 
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.util.LinkedList;
@@ -11,25 +13,31 @@ import java.util.LinkedList;
  * @since 2019/4/25 19:55
  */
 public class Application {
-  public static void main(String[] args) throws IOException, GitAPIException {
+  public static void main(String[] args) throws IOException, GitAPIException, IOException, SAXException, ParserConfigurationException {
 
-    LinkedList<File> list = FileUtils.getSubFolders(new File("D:\\git\\jsh-bak"));
-    for (File file:list) {
+    //LinkedList<File> list = FileUtils.getSubFolders(new File("D:\jsh\jsh\jsh-service-log-provider"));
+    File file = new File("D:\\jsh\\jsh\\jsh-service-log-provider");
+    PomUtils obj = new PomUtils(file);
+    System.out.println(obj.getVersion());
+/*    for (File file:list) {
       GitUtils gu = new GitUtils(file);
       System.out.println("Project 获取最新的Tag 信息");
       System.out.println(file.getName() + "----" + gu.getLastTag().name);
 
 
-      /*System.out.println("DEV分支从远程拉去代码并合并");
+      *//*System.out.println("DEV分支从远程拉去代码并合并");
       System.out.println(gu.branchPull("dev"));
       System.out.println("Master分支从远程拉去代码并合并");
-      System.out.println(gu.branchPull("master"));*/
+      System.out.println(gu.branchPull("master"));*//*
 
 
       System.out.println("将dev分支的代码合并到master分支");
       System.out.println(gu.mergeBranch("dev","master"));
       System.out.println("将master分支的代码合并到dev分支");
       System.out.println(gu.mergeBranch("master","dev"));
-    }
+    }*/
+
+
+
   }
 }
--- src/main/java/com/blogs/PomUtils.java
+++ src/main/java/com/blogs/PomUtils.java
@@ -8,6 +8,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -19,16 +20,66 @@ import org.xml.sax.SAXException;
  * @since 2019/4/28 17:46
  */
 public class PomUtils {
+  protected static Document xmldoc;
+
+  PomUtils(File projectDir) throws ParserConfigurationException, IOException, SAXException{
+    File pomFile = new File(projectDir.getAbsolutePath() +  System.getProperty("file.separator") + "pom.xml");
+    System.out.println(pomFile);
+    DocumentBuilderFactory fac = DocumentBuilderFactory.newInstance();
+    //使用工程，创建一个文档解析器
+    DocumentBuilder builder = fac.newDocumentBuilder();
+    //使用文档解析器解析一个文件，放到document对象中
+    xmldoc = builder.parse(pomFile);
+  }
   public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
     System.out.println("hello");
-    File f = new File("../pom.xml");
+    File f = new File("pombak.xml");
     //创建一个文档解析器工厂
     DocumentBuilderFactory fac = DocumentBuilderFactory.newInstance();
     //使用工程，创建一个文档解析器
     DocumentBuilder builder = fac.newDocumentBuilder();
     //使用文档解析器解析一个文件，放到document对象中
-    Document doc = builder.parse(f);
-    doc.getDocumentElement();
+    Document xmldoc = builder.parse(f);
+    Element element = xmldoc.getDocumentElement();
+    NodeList list = xmldoc.getElementsByTagName("version");
 
+    System.out.println(xmldoc);
+    System.out.println(xmldoc.getNodeType());
+    System.out.println(xmldoc.getDocumentElement());
+    System.out.println(xmldoc.getDocumentElement().getNodeType());
+    System.out.println(xmldoc.getDocumentElement().getFirstChild().getNodeType());
+    System.out.println(xmldoc.getDocumentElement().getFirstChild().getNextSibling());
+    System.out.println(xmldoc.getDocumentElement().getFirstChild().getNextSibling().getNodeType());
+    System.out.println(xmldoc.getDocumentElement().getFirstChild().getNextSibling().getNextSibling().getNextSibling());
+
+    Node node = element.getFirstChild();
+    while (node.getNodeType() > 0){
+      System.out.println(node.getNodeName());
+      node = node.getNextSibling();
+      System.out.println("==");
+    }
+    System.out.println(xmldoc.getDocumentElement().getLastChild().getNextSibling());
+    System.out.println("----------------");
+
+    for (int i = 0;i < list.getLength();i++) {
+      System.out.println(list.item(i).getTextContent());
+    }
+  }
+
+
+
+  public static String getVersion(){
+    System.out.println("hello");
+    Element element = xmldoc.getDocumentElement();
+    Node node = element.getFirstChild();
+    while (node.getNodeType() > 0) {
+      System.out.println(node.getNodeName());
+      if ("version".equals(node.getNodeName())) {
+        System.out.println(node.getTextContent());
+        return node.getTextContent();
+      }
+      node = node.getNextSibling();
+    }
+    return "NotFound";
   }
 }
