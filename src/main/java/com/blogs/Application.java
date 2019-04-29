9a248fe7e94c93c0753187ff0cef63933619b1d9
admin <seeyouon26@outlook.com>
2019-04-29 22:33:08 +0800

    DOM xml 获取POM文件依赖，更新POM文件version和依赖version

--- src/main/java/com/blogs/Application.java
+++ src/main/java/com/blogs/Application.java
@@ -1,19 +1,23 @@
 package com.blogs;
 
+import com.sun.xml.internal.ws.wsdl.parser.MemberSubmissionAddressingWSDLParserExtension;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 /**
  * @author lyzhang
  * @since 2019/4/25 19:55
  */
 public class Application {
-  public static void main(String[] args) throws IOException, GitAPIException, IOException, SAXException, ParserConfigurationException {
+  public static void main(String[] args) throws IOException, GitAPIException, IOException, SAXException, ParserConfigurationException, TransformerException {
 
     LinkedList<File> list = FileUtils.getSubFolders(new File("D:\\git\\jsh-bak"));
     //File file = new File("D:\\jsh\\jsh\\jsh-service-log-provider");
@@ -23,8 +27,14 @@ public class Application {
       File pomFile = new File(file.getAbsolutePath() +  System.getProperty("file.separator") + "pom.xml");
       if (pomFile.exists()) {
         PomUtils obj = new PomUtils(pomFile);
-        System.out.println(file.getName());
-        System.out.println(file.getName() + " ---"  +obj.getDependency());
+        //System.out.println(file.getName() + " ---"  +obj.getDependency());
+        System.out.println(obj.getVersion());
+        System.out.println(obj.setVersion("2.0.0"));
+        System.out.println(obj.getVersion());
+        System.out.println(obj.getDependency());
+        Map<String,String> dependencies = new HashMap<String,String>();
+        dependencies.put("dubbo.version","3.0.0");
+        System.out.println(obj.setDependency(dependencies));
       } else {
         System.out.println(file.getName() + " is not maven project");
       }
--- src/main/java/com/blogs/PomUtils.java
+++ src/main/java/com/blogs/PomUtils.java
@@ -2,9 +2,18 @@ package com.blogs;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -18,6 +27,7 @@ import org.xml.sax.SAXException;
  */
 public class PomUtils {
   protected static Document xmldoc;
+  protected static File f;
 
   PomUtils(File pomFile) throws ParserConfigurationException, IOException, SAXException{
     //System.out.println(pomFile);
@@ -25,6 +35,7 @@ public class PomUtils {
     //使用工程，创建一个文档解析器
     DocumentBuilder builder = fac.newDocumentBuilder();
     //使用文档解析器解析一个文件，放到document对象中
+    f = pomFile;
     xmldoc = builder.parse(pomFile);
   }
   public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
@@ -76,15 +87,58 @@ public class PomUtils {
     return "NotFound";
   }
 
-  public static String[][] getDependency () {
-    String[][] dependencies = {{"rows","fields"}};
+  public static boolean setVersion (String projectVersion) throws TransformerException {
+    Element element = xmldoc.getDocumentElement();
+    Node node = element.getFirstChild();
+    boolean change = false;
+    while (node != null) {
+      //System.out.println(node.getNodeName());
+      if ("version".equals(node.getNodeName())) {
+        //System.out.println(node.getTextContent());
+        node.setTextContent(projectVersion);
+        change = true;
+        break;
+      }
+      node = node.getNextSibling();
+    }
+    if (change) {
+      TransformerFactory factor = TransformerFactory.newInstance();
+      Transformer former = factor.newTransformer();
+      former.transform(new DOMSource(xmldoc),new StreamResult(f));
+    }
+    return true;
+  }
+
+  public static Map<String, String> getDependency () {
+    Map<String,String> dependencies = new HashMap<String,String>();
     NodeList nodesDepend = xmldoc.getElementsByTagName("properties");
     //System.out.println(nodesDepend.getLength());
     Node dependency = nodesDepend.item(0).getFirstChild();
     while (dependency != null) {
-      System.out.println(dependency.getNodeName() + "---" + dependency.getTextContent());
+      dependencies.put(dependency.getNodeName(),dependency.getTextContent());
       dependency = dependency.getNextSibling();
     }
     return dependencies;
+  }
+
+  public static boolean setDependency (Map<String,String> dependencies) throws TransformerException{
+    NodeList nodesDepend = xmldoc.getElementsByTagName("properties");
+    //System.out.println(nodesDepend.getLength());
+    Node dependency = nodesDepend.item(0).getFirstChild();
+    boolean change = false;
+    while (dependency != null) {
+      String elementName = dependency.getNodeName();
+      if (dependencies.containsKey(elementName)) {
+        dependency.setTextContent(dependencies.get(elementName));
+        change = true;
+      }
+      dependency = dependency.getNextSibling();
+    }
+    if (change) {
+      TransformerFactory factor = TransformerFactory.newInstance();
+      Transformer former = factor.newTransformer();
+      former.transform(new DOMSource(xmldoc),new StreamResult(f));
+    }
+    return true;
   }
 }
